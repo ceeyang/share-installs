a66953d0a163f919cfd4b17f6263373be7d09474
ceeyang <ceeyang@admin.com>
2026-04-12 22:39:00 +0700

    fix(sdk-js): add missing eslint config and cleanup redundant ci

---
+++ sdk/js/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+};
